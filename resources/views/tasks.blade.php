156ea15a0fa2bd62a3f02270a16a6d64ddfd4a02
DESKTOP-MH088Q2\Lenovo 2020 <louisversace@yahoo.co.uk>
2021-07-13 21:45:52 +0100

    Main template added

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -6,11 +6,26 @@
     <title>MLP To-Do</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
 </head>
 <body>
+
+<div class="main-content">
+    <div class="mlp-logo">
+        <img src="/assets/logo.png">
+    </div>
+    <div class="content">
+        <div class="content-left">
+
+        </div>
+        <div class="content-right">
+
+        </div>
+    </div>
+
+</div>
 
 </body>
 </html>
